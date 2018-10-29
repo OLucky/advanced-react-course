a3abb46fcd1bc84c979df0abe10ef341f2bc00c6
Georgii Ishchenko <ge.ishchenko@gmail.com>
2018-10-29 18:27:47 +0200

    Finish module 3

---
+++ backend/src/db.js
@@ -0,0 +1,10 @@
+//This file connect to remote prisma DB to query with JS
+const {Prisma} = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET
+});
+
+module.exports = db;
